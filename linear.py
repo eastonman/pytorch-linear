864ebce050b430adbde3d65ccd9b45c1f694126e
Easton Man <easton@techminer.org>
2020-10-05 21:58:12 +0800

    init

---
+++ linear.py
@@ -0,0 +1,63 @@
+from torch import nn
+import torch
+from torch import Tensor
+
+
+class Model(nn.Module):
+    def __init__(self):
+        """
+        In the constructor we instantiate two nn.Linear module
+        """
+        super(Model, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)  # One in and one out
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Variable of input data and we must return
+        a Variable of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Variables.
+        """
+        y_pred = self.linear(x)
+        return y_pred
+
+
+# our model
+model = Model()
+
+# Construct our loss function and an Optimizer. The call to model.parameters()
+# in the SGD constructor will contain the learnable parameters of the two
+# nn.Linear modules which are members of the model.
+criterion = torch.nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+with open("input.txt", 'r') as input:
+    total = int(input.readline())
+    x_raw = input.readline().split()
+    y_raw = input.readline().split()
+    x_float = [[float(x)] for x in x_raw[0:total]]
+    y_float = [[float(y)] for y in y_raw[0:total]]
+
+x_data = Tensor(x_float)
+y_data = Tensor(y_float)
+
+del x_raw, y_raw, x_float, y_float
+
+
+# Training loop
+for epoch in range(500):
+    # 1) Forward pass: Compute predicted y by passing x to the model
+    y_pred = model(x_data)
+
+    # 2) Compute and print loss
+    loss = criterion(y_pred, y_data)
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+# After training
+hour_var = Tensor([[10.0]])
+y_pred = model(hour_var)
+print("Prediction (after training)",  4, model(hour_var).data[0][0].item())
